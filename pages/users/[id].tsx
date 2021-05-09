1ca4b33d09775230f9579df611bdc6804131e0b1
Permadi Wibisono <mbapewe@gmail.com>
2021-05-10 05:45:34 +0700

    Updated ui users page

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,14 +1,17 @@
 import { NextPage } from "next"
 import { useRouter } from "next/router"
+import { Flex, Heading } from "@chakra-ui/react"
 
 const UserPage : NextPage = () => {
   const router = useRouter()
   const { id, ...rest } = router.query
   return (
-    <>
-      <h1>User: {id}</h1>
+    <Flex flexDirection="column" alignItems="center" margin={4}>
+      <Heading as="h1" size="2xl" marginY="2rem">
+        User: {id}
+      </Heading>
       <pre>query: {JSON.stringify(rest)}</pre>
-    </>
+    </Flex>
   )
 }
 
