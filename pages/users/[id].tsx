ae32bf4b82a7ec3ef83c158e71eaf4b667781183
Permadi Wibisono <mbapewe@gmail.com>
2021-05-18 00:50:12 +0700

    Fetch data from an api on the server side (getServerSideProps)

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,7 +1,13 @@
-import { NextPage } from "next"
-import { useRouter } from "next/router"
-import useSWR from "swr"
-import { Alert, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react"
+import { GetServerSideProps, NextPage } from "next"
+import ErrorPage from 'next/error'
+import { Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react"
+import fetch from 'node-fetch'
+
+type ErrorResponse = {
+  message: string;
+  status_code: number;
+}
+
 
 type DataResponse = {
   data: {
@@ -13,58 +19,82 @@ type DataResponse = {
   status_code: number;
 }
 
-const UserPage : NextPage = () => {
-  const router = useRouter()
-  const { id } = router.query
-  let user = null
-  const { data, error } = useSWR(`/api/users/${id}`, async (url: string) => {
-    const res = await fetch(url)
-    if(!res.ok){
-      throw Error("Error while fetching")
-    }
-    const data: DataResponse = await res.json()
-    return data
-  })
-  if(data){
-    user = data.data
+type Props = {
+  data: DataResponse;
+  error: ErrorResponse;
+}
+
+const UserPage : NextPage<Props> = (props) => {
+  const { error, data: { data } } = props
+  if(error){
+    return <ErrorPage statusCode={error.status_code}/>
   }
   return (
     <Flex flexDirection="column" alignItems="center" margin={4}>
       <Heading as="h1" size="2xl" marginY="2rem">
         User
       </Heading>
-      {error? (
-        <Alert status="error">
-          Oops, {error.message}
-        </Alert>
-      ): !user? (
-        <Alert status="info">
-          Loading...
-        </Alert>
-      ):(
-        <SimpleGrid columns={2} width="2xs" spacingY={4}>
-          <Text fontWeight="bold" marginRight={4}>
-            UserID
-          </Text>
-          <Text>
-            {user.id}
-          </Text>
-          <Text fontWeight="bold" marginRight={4}>
-            Name
-          </Text>
-          <Text>
-            {user.name}
-          </Text>
-          <Text fontWeight="bold" marginRight={4}>
-            Name
-          </Text>
-          <Text>
-            {user.email}
-          </Text>
-        </SimpleGrid>
-      )}
+      <SimpleGrid columns={2} width="2xs" spacingY={4}>
+        <Text fontWeight="bold" marginRight={4}>
+          UserID
+        </Text>
+        <Text>
+          {data.id}
+        </Text>
+        <Text fontWeight="bold" marginRight={4}>
+          Name
+        </Text>
+        <Text>
+          {data.name}
+        </Text>
+        <Text fontWeight="bold" marginRight={4}>
+          Name
+        </Text>
+        <Text>
+          {data.email}
+        </Text>
+      </SimpleGrid>
     </Flex>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async({params}) => {
+  try {
+    const { id } = params
+    const result = await fetch(`http://localhost:3000/api/users/${id}`)
+    const data = await result.json()
+    console.log("data: ", data)
+    let payload:  {data: DataResponse | {}, error: ErrorResponse}= {
+      data: {},
+      error: null
+    }
+    if(!data.error){
+      payload = {
+        ...payload,
+        error: null,
+        data
+      }
+    } else{
+      payload = {
+        ...payload,
+        error: { ...data.error },
+      }
+    }
+    return {
+      props: { ...payload }
+    }
+  } catch(e){
+    console.log("catch data: ", e)
+    return {
+      props: {
+        data: {},
+        error: {
+          status_code: 500,
+          message: "Internal Server Error"
+        }
+      }
+    }
+  }
 }
 
 export default UserPage
