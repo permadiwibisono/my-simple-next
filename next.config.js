2ca9cfcc885d15cb22a19f413e1028ecbfe75c4e
Permadi Wibisono <mbapewe@gmail.com>
2021-05-18 02:22:45 +0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
-require("dotenv").config()
+const { parsed } = require("dotenv").config()
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 
 module.exports = (phase, { env }) => {
-  console.log("env:", env)
+  console.log("env:", env, parsed)
   const isDev = phase === PHASE_DEVELOPMENT_SERVER
   const SERVER_URI = isDev? "http://localhost:3000": "https://example.org"
   return {
     env: {
-      REDIRECT_URI: `${SERVER_URI}/api/callback`,
-      POST_LOGOUT_REDIRECT_URI: `${SERVER_URI}/logout`,
+      ...parsed,
+      REDIRECT_URI: `/api/callback`,
+      POST_LOGOUT_REDIRECT_URI: `/secret`,
       AUTH0_SCOPE: "openid profile",
       SERVER_URI
     }
